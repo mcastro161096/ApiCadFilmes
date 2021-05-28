d138778ccf61ded40aee243650288e11513ba31a
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-05-28 13:27:49 -0300

    Criando interface e implemetação de serviço para filme.

---
+++ Domain/Models/IServices/IFilmeService.cs
@@ -0,0 +1,17 @@
+﻿using ApiCadFilmes.Domain.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCadFilmes.Domain.Models.IServices
+{
+    interface IFilmeService
+    {
+        Task<IEnumerable<Filme>> ListAsync();
+        Task AddAsync(Filme filme);
+        Task<Filme> FindByIdAsync(int id);
+        void Update(Filme filme);
+        void Remove(Filme filme);
+    }
+}
---
+++ Domain/Services/FilmeService.cs
@@ -0,0 +1,37 @@
+﻿using ApiCadFilmes.Domain.Models.Entities;
+using ApiCadFilmes.Domain.Models.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCadFilmes.Domain.Services
+{
+    public class FilmeService : IFilmeService
+    {
+        public Task AddAsync(Filme filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Filme> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Filme>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Filme filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Filme filme)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
