79780523d76f821acec6b401f44fd1ef1d21359c
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-05-28 23:17:43 -0300

    Criando interface e implemetação de filmeRepository que irá de fato acessar o banco.

---
+++ Domain/Repository/BaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApiCadFilmes.Domain.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCadFilmes.Domain.Repository
+{
+    public abstract class BaseRepository
+    {
+        protected readonly ApiContext _context;
+
+        public BaseRepository(ApiContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Domain/Repository/FilmeRepository.cs
@@ -0,0 +1,41 @@
+﻿using ApiCadFilmes.Domain.Models.Context;
+using ApiCadFilmes.Domain.Models.Entities;
+using ApiCadFilmes.Domain.Models.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCadFilmes.Domain.Repository
+{
+    public class FilmeRepository : BaseRepository, IFilmeRepository
+    {
+        public FilmeRepository(ApiContext context) : base(context) {}
+        
+
+        public Task AddAsync(Filme filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Filme> FindByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Filme>> ListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Filme filme)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Filme filme)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
