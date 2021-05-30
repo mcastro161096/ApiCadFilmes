ef2da5ccc824477aca86678531e117353be3d3d6
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-05-29 21:55:34 -0300

    Criando interface e implementação para padronizar mensagens.

---
+++ Domain/Models/IServices/IMessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCadFilmes.Domain.Models.IServices
+{
+    public interface IMessageService
+    {
+        public string MsgPreencherCampoVazioObrigatorio(string nomeCampo);
+
+        public string MsgTamanhoMinEMaxDoCampo(string nomeCampo);
+
+        public string MsgBooleanoNaoPodeSerNulo(string nomeCampo);
+    }
+}
